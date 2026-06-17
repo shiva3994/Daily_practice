d46309787008ec3428d3c223c69fb5e2994a18cd
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-17 13:06:21 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -156,4 +156,51 @@ select
 from patients
 where first_name like "s%____%s";
 
+-- Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list.
+-- If only 1 person is named 'Leo' then include them in the output.
+
+select
+		first_name
+from patients
+group by first_name
+having COUNT(first_name) = 1;
+
+-- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+-- Primary diagnosis is stored in the admissions table.
+
+select
+	  p.patient_id,
+      first_name,
+      last_name
+from patients p
+
+join admissions a
+on a.patient_id = p.patient_id
+
+where diagnosis = 'Dementia';
+
+-- Display every patient's first_name.
+-- Order the list by the length of each name and then by alphabetically.
+
+select
+		first_name
+from patients
+order by len(first_name),
+		 first_name;
+         
+-- Show the total amount of male patients and the total amount of female patients in the patients table.
+-- Display the two results in the same row.
+
+select 
+		(select count(*) from patients where gender = 'M') AS male_count,
+        (select count(*) from patients where gender = 'F') AS female_count;
+
+-- OR
+
+select 
+  SUM(Gender = 'M') AS male_count, 
+  SUM(Gender = 'F') AS female_count
+from patients;
+
 
