959a3f34b332f8774cd653262869e0c749017995
SHIVA THAKUR <shiv39941@gmail.com>
2026-06-28 01:30:07 +0530

    Update Online Practice SQL.sql

--- Daily SQL/Online Practice SQL.sql
+++ Daily SQL/Online Practice SQL.sql
@@ -589,3 +589,39 @@ WHERE a.admission_date = (SELECT
                           		MAX(a2.admission_date)
     					  FROM admissions a2
     					  WHERE a2.patient_id = p.patient_id);
+
+-- Show all of the patients grouped into weight groups.
+-- Show the total amount of patients in each weight group.
+-- Order the list by the weight group decending.
+-- For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+
+select
+		count(*) as patients_in_group,
+        floor(weight/10)* 10 as weight_group
+        
+from patients
+group by weight_group
+order by weight_group desc;
+
+-- Show patient_id, weight, height, isObese from the patients table.
+-- Display isObese as a boolean 0 or 1.
+-- Obese is defined as weight(kg)/(height(m)2) >= 30.
+-- weight is in units kg.
+-- height is in units cm.
+
+SELECT 
+		patient_id,
+        weight,
+        height, 
+  		(CASE 
+         		WHEN weight/(POWER(height/100.0,2)) >= 30
+         		THEN 1	
+         		ELSE 0
+      			END) AS isObese
+FROM patients;
+
+-- If you divide an int by an int you will get an int. Dividing an int by a float will return a float.
+-- That's why you have to divide by 100.0 and not 100.
+-- Use CAST(variable_name AS FLOAT) function if you are dividing by two variables.
+
+-- ************************************************************************************************ --
